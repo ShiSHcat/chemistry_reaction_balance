f33fa6c091fd6e8badf34b318456e92ee97361d5
ShiSHcat <shishcat.dp@gmail.com>
2023-10-27 16:18:10 +0200

    Remove useless token

--- discord_bot/balance_api_calls.js
+++ discord_bot/balance_api_calls.js
@@ -1,5 +1,4 @@
 import fetch from 'node-fetch';
-const apiToken = "STITTOL1249122";
 
 const balanceReaction = async (reaction) => {
     const url = `http://127.0.0.1:8000/balance_reaction?reaction=${encodeURIComponent(reaction)}}`;
